9f176e2967f7a46ea6ecf5db71db4ff871d050a6
Tom <cresswellthomas@yahoo.co.uk>
2022-02-13 19:21:15 +0000

    Ball physics now work, boundaries are still an issue

--- Assets/Scripts/Ball Scripts/BallControl.cs
+++ Assets/Scripts/Ball Scripts/BallControl.cs
@@ -7,12 +7,20 @@ public class BallControl : MonoBehaviour
     private bool ballMode; //false if ball is attached to platofrm, true if ball is in play
     private Vector3 ballPos;
     private Vector2 ballInitialVector;
+    private Rigidbody2D rb2D;
+    public float gameBounds;
+
+    private void Awake()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
 
     // Start is called before the first frame update
     void Start()
     {
         //Create initial force
-        ballInitialVector = new Vector2(10.0f, 30.0f);
+        ballInitialVector = new Vector2(200.0f, 400.0f);
 
         ballMode = false;
 
@@ -22,14 +30,28 @@ public class BallControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Inital force to start the game
         if (Input.GetButtonDown("Jump") == true) 
         {
             if (!ballMode)
             {
-                //Rigidbody2D.AddForce(ballInitialVector);
+                rb2D.AddForce(ballInitialVector);
 
                 ballMode = !!ballMode;
             }
+        }
+
+        //Update player position
+        ballPos = this.transform.position;
+
+        //Prevent platform from moving outside game bounds
+        if (ballPos.x < -gameBounds)
+        {
+            transform.position = new Vector3(-gameBounds, ballPos.y, ballPos.z);
+        }
+        if (ballPos.x > gameBounds)
+        {
+            transform.position = new Vector3(gameBounds, ballPos.y, ballPos.z);
         }
     }
 }
