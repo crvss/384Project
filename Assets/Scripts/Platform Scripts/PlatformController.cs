830271693e75f651d2e271e38085f829bfa5e31b
Tom <cresswellthomas@yahoo.co.uk>
2022-02-13 14:00:51 +0000

    Platform movement fixed

--- Assets/Scripts/Platform Scripts/PlatformController.cs
+++ Assets/Scripts/Platform Scripts/PlatformController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlatformController : MonoBehaviour
 {
+    [SerializeField] private float Speed = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +15,8 @@ public class PlatformController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A)) {
-            Debug.Log("A");
-        }
+        float hSpeed = Input.GetAxis("Horizontal") * Speed;
+
+        this.transform.Translate(hSpeed * Time.deltaTime, 0, 0); //use time to move player on seconds
     }
 }
