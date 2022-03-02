cdf2c8c7054e56f607c81b42546ddef13ac22844
Tom <cresswellthomas@yahoo.co.uk>
2022-03-02 19:24:50 +0000

    Brick script made and initial methods made

--- Assets/Scripts/Brick Scripts/BrickControl.cs
+++ Assets/Scripts/Brick Scripts/BrickControl.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class BrickControl : MonoBehaviour
 {
+    public int brickHP;
+    public int brickDamageTaken;
+    public int pointsGiven;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        brickDamageTaken = 0;
     }
 
     // Update is called once per frame
