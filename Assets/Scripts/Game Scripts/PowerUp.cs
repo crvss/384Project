42e55c5301a38af09332d11f13cb889e3fe22f35
Tom <cresswellthomas@yahoo.co.uk>
2022-04-22 16:11:55 +0100

    Fixed bug in level 3 for death menu

--- Assets/Scripts/Game Scripts/PowerUp.cs
+++ Assets/Scripts/Game Scripts/PowerUp.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class PowerUp : MonoBehaviour
 {
-    // Update is called once per frame
+    private Rigidbody2D rb2D;
+
+    private void Awake()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
         transform.position -= transform.up * Time.deltaTime * 5;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(this.gameObject);
     }
 }
