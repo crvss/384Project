bcee015bc3a320dad97ae14afffb089d52160199
Tom <cresswellthomas@yahoo.co.uk>
2022-04-25 13:04:41 +0100

    Added start of Achievement file

--- Assets/Scripts/Achievement Scripts/Achievements.cs
+++ Assets/Scripts/Achievement Scripts/Achievements.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Achievements : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject achievePanel;
+    public AudioSource achieveSound;
 
-    // Update is called once per frame
+    public bool achieveActive = false;
+
+    public GameObject acieveBrickBreakerTitle;
+    public GameObject achieveBrickBreakerDesc;
+    public GameObject achieveBrickBreakerImage;
+    public static int achieveBrickBreakerNumber;
+    public int achieveBrickBreakerTrigger = 20;
+    public int achieveBrickBreakerCode;
+
     void Update()
     {
         
