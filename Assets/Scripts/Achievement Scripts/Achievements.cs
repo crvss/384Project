a595472156554e4ef6a74edbdf2d6ea3c8ce5456
Tom <cresswellthomas@yahoo.co.uk>
2022-05-05 13:23:12 +0100

    Added Achievements Screen

--- Assets/Scripts/Achievement Scripts/Achievements.cs
+++ Assets/Scripts/Achievement Scripts/Achievements.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class Achievements : MonoBehaviour
 {
@@ -28,11 +27,6 @@ public class Achievements : MonoBehaviour
         {
             StartCoroutine(BBTrigger());
         }
-    }
-
-    public void ReturnToMenu()
-    {
-        SceneManager.LoadScene("Main Menu");
     }
 
     IEnumerator BBTrigger()
