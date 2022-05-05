7e4c4d08a3259682af51af8267787f8f4f09037e
Tom <cresswellthomas@yahoo.co.uk>
2022-05-05 13:40:25 +0100

    Fixed Ach (ish) and added main menu button

--- Assets/Scripts/Achievement Scripts/AchievementsMenu.cs
+++ Assets/Scripts/Achievement Scripts/AchievementsMenu.cs
@@ -5,8 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class AchievementsMenu : MonoBehaviour
 {
+    public GameObject achieveState;
+    public static bool achieveFlag = false;
+
+    private void Awake()
+    {
+        if (achieveFlag)
+        {
+            UnlockAch();
+        }
+    }
+
     public void ReturnToMenu()
     {
         SceneManager.LoadScene("Main Menu");
+    }
+
+    public void UnlockAch()
+    {
+        achieveState.GetComponent<TMPro.TextMeshProUGUI>().text = "Achievement Unlocked";
     }
 }
